bd5e467b1d52c23abe9ff5ba766b4c07aa877f03
mih-prog <mihprogs@gmail.com>
2021-10-19 23:00:02 +0300

    v1

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
-import discord
+from discord.ext import commands
+import config
+config = config()
+
+
+def get_prefix(bot, msg):
+    prefixes = [config.prefix, f'<@{bot.user.id}>']
+    # Your bot prefix(s)
+
+    return commands.when_mentioned_or(*prefixes)(bot, msg)
+
+
+bot = commands.Bot(command_prefix=get_prefix, description='')
+exts = ['cogs.music']  # Add your Cog extensions here
+
+
+for i in exts:
+    bot.load_extension(i)
+    print(f'cog: {i} connect')
+
+def run():
+    bot.run(config.get('token'))
 
 if __name__ == '__main__':
-    print('posel naher, test')
+    run()
---
+++ modules/config.py
@@ -0,0 +1,37 @@
+import json, io, os
+
+
+class configs():
+    def __init__(self, confDir):
+        self.DbDerectoryName = confDir
+
+    def Get(self, path):
+        if not os.path.isdir(self.DbDerectoryName + '/' + path):
+            return None
+        else:
+            if not os.path.isfile(self.DbDerectoryName + '/' + path + '/data'):
+                return None
+            else:
+                file = io.FileIO(self.DbDerectoryName+'/'+path+'/data')
+                file.seek(0)
+                try:data = json.loads(file.read().decode())
+                except:file.seek(0); file.write(b'{}'); data = {}
+                file.close()
+                return data
+
+    def Set(self, path, data):
+        if not os.path.isdir(self.DbDerectoryName + '/' + path):
+            os.makedirs(self.DbDerectoryName + '/' + path)
+            file = io.FileIO(self.DbDerectoryName+'/'+path+'/data', 'w+')
+            file.write(json.dumps(data).encode())
+            file.close()
+        else:
+            if not os.path.isfile(self.DbDerectoryName + '/' + path + '/data'):
+                file = io.FileIO(self.DbDerectoryName + '/' + path + '/data', 'w+')
+                file.write(json.dumps(data).encode())
+                file.close()
+            else:
+                file = io.FileIO(self.DbDerectoryName + '/' + path + '/data', 'w+')
+                file.seek(0)
+                file.write(json.dumps(data).encode())
+                file.close()
